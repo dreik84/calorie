c658fe1d6027851905d4afbad8ef962cc3022e01
dreik84 <dreik84@mail.ru>
2025-07-16 10:51:45 +0300

    Add StepTracker.java

---
+++ src/main/java/com/example/StepTracker.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.HashMap;
+
+public class StepTracker {
+
+    private int purpose = 1000;
+    HashMap<Integer, MonthData> monthToData = new HashMap<>();
+
+    public StepTracker() {
+        for (int i = 0; i < 12; i++) {
+            monthToData.put(i, new MonthData());
+        }
+    }
+
+    public int getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(int purpose) {
+        this.purpose = purpose;
+    }
+
+    class MonthData {
+
+        // TODO
+    }
+}
