28ed911faef0da82492281dd93f0fc12218dbc64
dreik84 <dreik84@mail.ru>
2025-07-21 16:27:52 +0300

    Update Converter.java, fix method names

--- src/main/java/com/example/Converter.java
+++ src/main/java/com/example/Converter.java
@@ -2,11 +2,11 @@ package com.example;
 
 public class Converter {
 
-    public static int stepsToMeters(int steps) {
-        return (int) Math.round(steps * 0.75);
+    public static int stepsToKilometers(int steps) {
+        return (int) Math.round(steps * 0.75 / 1000);
     }
 
-    public static int stepsToCalories(int steps) {
+    public static int stepsToKiloCalories(int steps) {
         return steps * 50 / 1000;
     }
 }
